fc7207d251abdfa778bb5a664f8ff84eecaf1950
paullin86 <ppaul80025@gmail.com>
2021-05-24 08:31:32 +0800

    add Graphy testing data

--- streamgraph_19Cov_20210523.R
+++ streamgraph_19Cov_20210523.R
@@ -13,15 +13,19 @@ data <- data.frame(
 )
 str(data)
 library(readr)
-result_ori <- read_csv("data/CDC_19CoV_day_20210522_133417_Big5.csv", 
+data_19CoV <- read_csv("data/CDC_19CoV_day_20210522_133417_Big5.csv", 
                        col_types = cols(.default = col_character()), 
                        locale = locale(encoding = "BIG5"))# View(result)
-
+colnames(data_19CoV);str(data_19CoV)
+data_19CoV$'確定病例數' <- as.integer(data_19CoV$'確定病例數')
+data_19CoV$'個案研判日' <- as.Date(data_19CoV$'個案研判日')
 # Stream graph with a legend
 pp <- streamgraph(data, key="name", value="value", date="year", height="300px", width="1000px") %>%
   sg_legend(show=TRUE, label="names: ")
-
+pp <- streamgraph(result_ori, key="縣市", value="確定病例數", date="個案研判日", height="300px", width="1000px") %>%
+  sg_legend(show=TRUE, label="縣市: ")
+pp
 
 # save the widget
 library(htmlwidgets)
-saveWidget(pp, file=paste0( getwd(), "/HtmlWidget/streamgraphDropdown.html")) # 
+saveWidget(pp, file=paste0( getwd(), "/HtmlWidget/streamgraph_19Cov_20210523.html")) # 
